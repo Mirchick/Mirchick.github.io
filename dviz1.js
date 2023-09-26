2c38108a8ede28284f8c6056314a9dea40bae669
Mirchick <144796955+Mirchick@users.noreply.github.com>
2023-09-27 00:39:22 +0300

    commit7

---
+++ dviz1.js
@@ -0,0 +1,39 @@
+const button11 = document.querySelector('.text2');
+function changeColor11() {
+	button11.style.background='white';
+}
+
+const button12 = document.querySelector('.text2');
+function changeColor12() {
+	button12.style.background='#8BA8FF';
+}
+
+const button21 = document.querySelector('.text3');
+function changeColor21() {
+	button21.style.background='white';
+}
+
+const button22 = document.querySelector('.text3');
+function changeColor22() {
+	button22.style.background='#8BA8FF';
+}
+
+const button31 = document.querySelector('.text4');
+function changeColor31() {
+	button31.style.background='white';
+}
+
+const button32 = document.querySelector('.text4');
+function changeColor32() {
+	button32.style.background='#8BA8FF';
+}
+
+const button41 = document.querySelector('.text5');
+function changeColor41() {
+	button41.style.background='white';
+}
+
+const button42 = document.querySelector('.text5');
+function changeColor42() {
+	button42.style.background='#8BA8FF';
+}
