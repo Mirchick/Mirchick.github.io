ac27b61111c7a9b5455a4dad6b9128eb693c43da
Mirchick <144796955+Mirchick@users.noreply.github.com>
2023-10-17 00:55:15 +0300

    commit9

--- dviz1.js
+++ dviz1.js
@@ -1,39 +1,20 @@
-const button11 = document.querySelector('.text2');
-function changeColor11() {
-	button11.style.background='white';
+const collection = document.getElementsByClassName("opisanieObjects")
+
+function changeColor1() {
+  for (let elem of collection){
+    elem.style.backgroundColor = "white";
+  }
 }
 
-const button12 = document.querySelector('.text2');
-function changeColor12() {
-	button12.style.background='#8BA8FF';
+function changeColor2() {
+  for (let elem of collection){
+    elem.style.backgroundColor = "#8BA8FF";
+  }
 }
 
-const button21 = document.querySelector('.text3');
-function changeColor21() {
-	button21.style.background='white';
-}
-
-const button22 = document.querySelector('.text3');
-function changeColor22() {
-	button22.style.background='#8BA8FF';
-}
-
-const button31 = document.querySelector('.text4');
-function changeColor31() {
-	button31.style.background='white';
-}
-
-const button32 = document.querySelector('.text4');
-function changeColor32() {
-	button32.style.background='#8BA8FF';
-}
-
-const button41 = document.querySelector('.text5');
-function changeColor41() {
-	button41.style.background='white';
-}
-
-const button42 = document.querySelector('.text5');
-function changeColor42() {
-	button42.style.background='#8BA8FF';
+function askUserName() {
+  let person = prompt("Пожалуйста, введите Ваше имя:", "Гость");
+  if (person != null) {
+    alert("Привет, " + person + "! Как дела?");
+  }
 }
